e0939ef93a709b03867d0e5fa08256a574931ae3
Susmi701 <susmiels590@gmail.com>
2024-05-14 12:41:57 +0530

    Add Article listing feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,12 @@
 class ArticlesController <ApplicationController
+
     def show
         # debugger
     @article = Article.find(params[:id])
+    end
+
+    def index
+        #debugger
+        @articles = Article.all
     end
 end
