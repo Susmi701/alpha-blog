ab7f81d45b48371b6ba1d1fb0755f268eac365ae
Susmi701 <susmiels590@gmail.com>
2024-05-27 16:56:05 +0530

    Add User Create form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
   resources :articles 
+  resources :users,except: [:new]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
