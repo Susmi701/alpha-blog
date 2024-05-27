35cc77254d89564ecda8b9bde53ede903a198b10
Susmi701 <susmiels590@gmail.com>
2024-05-27 11:22:28 +0530

    Add User table and model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
-class Article < ApplicationRecord
+class Article   < ApplicationRecord
     validates :title, presence: true,length: {minimum: 3,maximum: 100}
     validates :description, presence: true,length: {minimum: 10}
 end
